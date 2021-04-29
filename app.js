1ef5769bba5c09be67914acedd4aa6bbd0e174e7
bruno-costanzo <dev.bcostanzo@gmail.com>
2021-04-29 20:33:08 -0300

    project finished!

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
-// Define basic operations
-
-const sum = (a, b) => a + b;
-const multiply = (a, b) => a * b;
-const divide = (a, b) => a;
-const substract = (a, b) => a - b;
-const minus = (a) => -a;
-
 // buttons selectors
+const calcBtn = document.querySelectorAll('.calc-btn');
+const miniScreen = document.querySelector('.miniScreen');
 const screen = document.querySelector('#screen');
+
+// actions buttons
+const actionButtons = document.querySelectorAll('.actionbut');
+const cleanButton = document.querySelector('.clean');
+const onButton = document.querySelector('.on-button');
+const mirror = document.querySelector('#mirror');
+
+// numb buttons
 const n0 = document.querySelector('#n0');
 const n1 = document.querySelector('#n1');
 const n2 = document.querySelector('#n2');
@@ -18,12 +20,30 @@ const n6 = document.querySelector('#n6');
 const n7 = document.querySelector('#n7');
 const n8 = document.querySelector('#n8');
 const n9 = document.querySelector('#n9');
+const decimalPoint = document.querySelector('.decimal');
 
 // operators selector
 
 const operators = document.querySelectorAll('.operator');
 
-const numbersButtons = [n0, n1, n2, n3, n4, n5, n6, n7, n8, n9];
+const numbersButtons = [
+  n0,
+  n1,
+  n2,
+  n3,
+  n4,
+  n5,
+  n6,
+  n7,
+  n8,
+  n9,
+  actionButtons[2],
+  decimalPoint,
+];
+
+// define ans
+let ans = 0;
+const ansButton = document.querySelector('#ans');
 
 // values-array
 let values = [];
@@ -32,10 +52,21 @@ let values = [];
 
 for (let button in numbersButtons) {
   numbersButtons[button].addEventListener('click', () => {
-    if (screen.textContent == 'Bienvenido') {
-      screen.textContent = '';
+    if (button == 10) {
+      screen.textContent = screen.textContent.substring(
+        0,
+        screen.textContent.length - 1
+      );
+    } else {
+      if (screen.textContent == 'Bienvenido' || screen.textContent == ans) {
+        screen.textContent = '';
+      }
+      if (button == 11) {
+        screen.textContent = screen.textContent + '.';
+      } else {
+        screen.textContent = screen.textContent + button.toString();
+      }
     }
-    screen.textContent = screen.textContent + button.toString();
   });
 }
 
@@ -43,26 +74,99 @@ for (let button in numbersButtons) {
 
 document.addEventListener('keydown', (e) => {
   if (0 <= e.key && e.key <= 9) {
-    if (screen.textContent == 'Bienvenido') {
+    if (screen.textContent == 'Bienvenido' || screen.textContent == ans) {
       screen.textContent = '';
     }
     screen.textContent = screen.textContent + e.key.toString();
+    for (let btn of calcBtn) {
+      if (e.key == btn.innerHTML) {
+        btn.classList.add('keydown');
+        setTimeout(() => {
+          btn.classList.toggle('keydown');
+        }, 100);
+      }
+    }
   }
 });
 
+function uploadToMiniScreen(digits, operator) {
+  if (operator == '=') {
+    miniScreen.innerText += digits + operator;
+    let number = eval(
+      miniScreen.innerText
+        .substring(0, miniScreen.innerText.length - 1)
+        .toString()
+    );
+    if (number % 1 !== 0) {
+      number = number.toFixed(2);
+    }
+    screen.textContent = number;
+    miniScreen.innerText = '';
+    ans = screen.textContent;
+  } else {
+    if (operator == 'x') {
+      operator = '*';
+    } else if (operator == '%') {
+      operator = '/';
+    }
+    miniScreen.innerText += digits + operator;
+    screen.innerText = '';
+  }
+  // screen.textContent = eval(
+  //   miniScreen.innerText
+  //     .substring(0, miniScreen.innerText.length - 1)
+  //     .toString()
+  // );
+}
+
 for (let operator of operators) {
-  operator.addEventListener('click', () => {
+  operator.addEventListener('click', (e) => {
     if (screen.textContent == 'Bienvenido' || screen.textContent == '') {
       screen.textContent = '';
     } else if (
-      screen.textContent[screen.textContent.length - 1] == '%' ||
-      screen.textContent[screen.textContent.length - 1] == '+' ||
-      screen.textContent[screen.textContent.length - 1] == '-' ||
-      screen.textContent[screen.textContent.length - 1] == 'x'
+      e.target.innerText == '%' ||
+      e.target.innerText == '+' ||
+      e.target.innerText == '-' ||
+      e.target.innerText == 'x' ||
+      e.target.innerText == '='
     ) {
-    } else {
+      uploadToMiniScreen(screen.textContent, e.target.innerText);
+    }
+    // uploadCount(e)
+    else {
       values.push(screen.textContent);
       screen.textContent = '';
     }
   });
 }
+
+// Answer button
+
+ansButton.addEventListener('click', () => {
+  if (screen.textContent == ans) {
+    screen.textContent = '';
+  }
+  if (ans !== 0) {
+    screen.textContent = screen.textContent + ans;
+  }
+});
+
+// clean button
+
+cleanButton.addEventListener('click', () => {
+  screen.textContent = '';
+  miniScreen.textContent = '';
+  ans = 0;
+});
+
+// on Button
+onButton.addEventListener('click', () => {
+  mirror.classList.toggle('onoff');
+  if (mirror.classList[0] !== 'onoff') {
+    screen.textContent = 'Bienvenido';
+    miniScreen.textContent = '';
+    ans = 0;
+  } else {
+    screen.textContent = 'Goodbye!';
+  }
+});
